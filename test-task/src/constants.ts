904f07125b76ae47a9def1f6ec7960dae76f77ab
Elena Shustova <alenyf-ka@mail.ru>
2022-06-16 12:44:53 +0300

    feat: add initial content

---
+++ test-task/src/constants.ts
@@ -0,0 +1,25 @@
+export const CONTENT = [
+	{
+		type: 'panel',
+		props: {
+			width: 500,
+			height: 200,
+			visible: true
+		},
+	},
+	{
+		type: 'label',
+		props: {
+			caption: 'test',
+			visible: false
+		},
+	},
+	{
+		type: 'button',
+		props: {
+			width: 100,
+			height: 50,
+			visible: true
+		},
+	}
+]
